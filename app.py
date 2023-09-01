8b8e0e2a5b20e390d3bc45b624ebb569088ac5a7
Petra Eriksen <petra@MVRCSIS-20155.mvrec.local>
2023-09-01 10:28:50 -0300

    criação db para simular banco de dados de usuários

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 from flask import Flask
+from db import db_usuarios
 
 app = Flask(__name__)
 
 #Criação de um aplicativo Flask - Cadastro de usuario
 
-# Dados fictícios para simular um banco de dados de usuários
-db_usuarios = [
-    {"id": 1, "name": "Maria", "email": "maria1@maria.com.br"},
-    {"id": 2, "name": "Pedro", "email": "jose2@jose.com.br"},
-    # ...
-]
 
 @app.route('/cadastrar', methods = ['POST'])
 def cadastrar():
---
+++ db.py
@@ -0,0 +1,7 @@
+# Dados fictícios para simular um banco de dados de usuários
+
+db_usuarios = [
+    {"id": 1, "name": "Maria", "email": "maria1@maria.com.br"},
+    {"id": 2, "name": "Pedro", "email": "jose2@jose.com.br"},
+    # ...
+]

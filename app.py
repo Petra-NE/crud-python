fe74259e42e6373919fbddd3e18b04062c05ca3c
Petra Eriksen <petra@MVRCSIS-20155.mvrec.local>
2023-09-21 22:42:41 -0300

    Adicionando .gitignore para ignorar o diretório _pycache_

--- app.py
+++ app.py
@@ -1,26 +1,48 @@
+from flask_restx import Api, Resource, fields
 from flask import Flask
+
 from db import db_usuarios
 
 app = Flask(__name__)
 
+api = Api(
+    app,
+    version='1.0',
+    title='API de Cadastro de Usuários',
+    description='API para cadastrar e gerenciar usuários',
+)
+
+ns = api.namespace('User', description='CRUD de Cadastro de Usuário')
+modelo = api.model('User', {
+    'id': fields.Integer, 
+    'name': fields.String,
+    'address': fields.String
+})
+
+@ns.route('/usuarios', methods=['GET'])
+class mostrar_usuarios(Resource):
+    def get(self):
+        '''Listar todos os usuários'''
+        return db_usuarios
+
 #Criação de um aplicativo Flask - Cadastro de usuario
+@ns.route('/cadastrar', methods = ['POST'])
+class cadastrar(Resource):
+    def post(self):
+        '''Cria um novo usuário'''
+        return 'Cadastro realizado com sucesso'
 
+@ns.route('/atualizar', methods = ['PUT'])
+class atualizar(Resource):
+    def put(self):
+        '''Atualiza usuário existente'''
+        return 'Atualizado com sucesso'
 
-@app.route('/cadastrar', methods = ['POST'])
-def cadastrar():
-    return 'Cadastro realizado com sucesso'
-
-@app.route('/usuarios', methods=['GET'])
-def mostrar_usuarios():
-    return db_usuarios
-
-@app.route('/atualizar', methods = ['PUT'])
-def atualizar():
-    return 'Atualizado com sucesso'
-
-@app.route('/excluir', methods=['DELETE'])
-def excluir():
-    return 'Usuário excluído'
+@ns.route('/excluir', methods=['DELETE'])
+class excluir(Resource):
+    def delete():
+        '''Excluir usuário'''
+        return 'Usuário excluído'
 
 
 
